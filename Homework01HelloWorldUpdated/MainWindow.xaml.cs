154123b70cf9f2affe0794e0d116f1a4e2fc0fb1
Alex Wu <alexwu5310@gmail.com>
2020-04-09 18:14:38 -0700

    Homework01HelloWorldUpdates

---
+++ Homework01HelloWorldUpdated/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Homework01HelloWorldUpdated
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void uxName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            updateSumit();
+        }
+
+        private void uxPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            updateSumit();
+        }
+
+        private void updateSumit()
+        {
+            if (uxName.Text == "" || uxPassword.Password == "" )
+            {
+                uxSubmit.IsEnabled = false;
+            }
+            else
+            {
+                uxSubmit.IsEnabled = true;
+            }
+        }
+    }
+}
