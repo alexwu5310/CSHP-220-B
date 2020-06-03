d05aa4b338ed6f905174d1cc7253b414a433dd42
Alex Wu <alexwu5310@gmail.com>
2020-06-03 00:41:15 -0700

    add HW6

---
+++ HW6BirthdayCardGenerator/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HW6BirthdayCardGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CustomForm(Models.CustomData customData)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("BirthdayCardSent", customData);
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+    }
+
+}
---
+++ HW6BirthdayCardGenerator/Models/CustomData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HW6BirthdayCardGenerator.Models
+{
+    public class CustomData
+    {
+        [Required(ErrorMessage = "Please enter From")]
+        public string Sender { get; set; }
+        [Required(ErrorMessage = "Please enter To")]
+        public string Recipient { get; set; }
+        [Required(ErrorMessage = "Please enter Message")]
+        public string Message { get; set; }
+    }
+}
